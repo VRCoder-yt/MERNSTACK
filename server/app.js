27e70986ae4dc8c7a82a2d7726e3e96e88e3d1cc
Rohit POS <webxgrafix@gmail.com>
2022-10-22 01:14:12 +0530

    mondo db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+//Mondo DB SSH key
+const DB = 'mongodb+srv://rohit:7IDrvLbfTJJcunUp@cluster0.prkjs37.mongodb.net/MERNSTACKOFFCIAL?retryWrites=true&w=majority'
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log(`Connection Successful`);
+}).catch((err) => console.log(`no connection`));
 
 //Middleware 
 const Middleware = (req, res, next) => {
@@ -11,13 +24,16 @@ const Middleware = (req, res, next) => {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 app.get('/about', (req, res) => {
     res.send('About us!')
 })
+
 app.get('/contact', (req, res) => {
     res.send('contact us!')
 })
-app.get('/login', Middleware, (req, res) => {
+
+app.get('/login', Middleware, (req, res) => { // usingf middleware commonly in login page
     console.log(`welcome to login page`)
     res.send('login!')
 })
