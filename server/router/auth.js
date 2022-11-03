90afe50c09c8cdde398882fd23b4b91e5d1e8dd9
Rohit Bhure <webxgrafix@gmail.com>
2022-11-03 08:58:39 +0530

    post

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const user = require('../model/userSchema')
 const router = express.Router()
 
 require("../db/conn")
@@ -8,6 +7,8 @@ const user = require("../model/userSchema")
 router.get('/', (req, res) => {
     res.send(`Hello world from server router.js`)
 })
+
+/* --- using promises -------
 
 router.post('/register', (req, res) => {
     // console.log(req.body)
@@ -36,4 +37,40 @@ router.post('/register', (req, res) => {
         }).catch(err => { console.log(err) })
 })
 
-module.exports = router 
+*/
+
+/* ---- using async / await ----- */
+
+router.post('/register', async (req, res) => {
+    // console.log(req.body)
+    // res.json({clintdata:req.body})
+
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "plz filled the feild properly" });
+    }
+
+    try {
+
+        const userExist = await user.findOne({ email: email })
+
+        if (userExist) {
+            return res.status(442).json({ error: "user already exist please fill properly" })
+        }
+
+        const user = new User({ name, email, phone, work, password, cpassword })
+        const userRegister = await user.save();
+
+        if (userRegister) {
+            res.status(201).json({ message: "user created successfuly" })
+        }
+        else {
+            res.status(500).json({ message: "failed to register" })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+module.exports = router
